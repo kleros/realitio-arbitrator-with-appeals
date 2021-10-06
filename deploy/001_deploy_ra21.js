9e325ea3fb1a9d61291d77031a7907118cc58e9f
Ferit Tunçer <ferit@cryptolab.net>
2021-10-06 14:27:16 +0300

    refactor(deploy): add mainnet contracts

--- deploy/001_deploy_ra21.js
+++ deploy/001_deploy_ra21.js
@@ -4,28 +4,30 @@ const fetch = require("node-fetch");
 module.exports = async ({ getNamedAccounts, deployments, getChainId, getUnnamedAccounts, ethers, config }) => {
   // INFURA_PROJECT_ID, PRIVATE_KEY and ETHERSCAN environment variables are required for this task. See Hardhat configuation for more information.
   const KLEROS = { 42: "0x60B2AbfDfaD9c0873242f59f2A8c32A3Cc682f80" };
-  const REALITIOv21 = { 42: "0x50E35A1ED424aB9C0B8C7095b3d9eC2fb791A168" };
-  const REALITIOv30 = { 42: "0xcB71745d032E16ec838430731282ff6c10D29Dea" };
+  const REALITIOv21 = { 1: "0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069", 42: "0x50E35A1ED424aB9C0B8C7095b3d9eC2fb791A168" };
+  const REALITIOv30 = { 1: "0x5b7dD1E86623548AF054A4985F7fc8Ccbb554E2c", 42: "0xcB71745d032E16ec838430731282ff6c10D29Dea" };
+
+  const primaryDocumentIPFSPath = "QmXyo9M4Z2XY6Nw9UfuuUNzKXXNhvt24q6pejuN9RYWPMr/Reality_Module_Governance_Oracle-Question_Resolution_Policy.pdf";
+  const templateHashes = [
+    "0x2630d91f54445fa42ec6738610b1a4c52b08a197156ab2ed97e4e17201528f2f",
+    "0xf006792a99a5de2495a30060356c53fdfd8cd04c53ad3402c5353035dc6b9589",
+    "0xe49d22c3fb3eac70dc2d9d76aa17b7e4b487f2f4b7c683f3705b638453520000",
+    "0x1d41a41b7d3550c01209c10495988bfa01dd2e86c5a3fae5f9dbc2ccde20c1a9",
+    "0x5b13a2ad9e4dfbf057ecf2c35d4152b61718a3ab46a14db63feb17b1515749ba",
+  ];
 
   const metadata = {
-    tos: "ipfs://Qme4jcJfBwXZ4j5fUcL9dwuWXkvFcJcZ7B6fRfwEsLoM5X/RealitioSafesnapPrimaryDoc.pdf",
-    template_hashes: [
-      "0x2630d91f54445fa42ec6738610b1a4c52b08a197156ab2ed97e4e17201528f2f",
-      "0xf006792a99a5de2495a30060356c53fdfd8cd04c53ad3402c5353035dc6b9589",
-      "0xe49d22c3fb3eac70dc2d9d76aa17b7e4b487f2f4b7c683f3705b638453520000",
-      "0x1d41a41b7d3550c01209c10495988bfa01dd2e86c5a3fae5f9dbc2ccde20c1a9",
-      "0x5b13a2ad9e4dfbf057ecf2c35d4152b61718a3ab46a14db63feb17b1515749ba",
-    ],
+    tos: `ipfs://${primaryDocumentIPFSPath}`,
   };
 
   const metaevidence = {
     category: "Oracle",
-    title: "A Realitio Question",
-    description: "A Realitio question has been raised to arbitration.",
+    title: "A reality.eth question",
+    description: "A reality.eth question has been raised to arbitration.",
     question: "Give an answer to the question.",
     evidenceDisplayInterfaceURI: "/ipfs/QmWCmzMB4zbzii8HV9HFGa8Evgt5i63GyveJtw2umxRrcX/reality-evidence-display-4/index.html",
     dynamicScriptURI: "/ipfs/QmWWsDmvjhR9UVRgkcG75vAKzfK3vB85EkZzudnaxwfAWr/bundle.js",
-    fileURI: "/ipfs/Qme4jcJfBwXZ4j5fUcL9dwuWXkvFcJcZ7B6fRfwEsLoM5X/RealitioSafesnapPrimaryDoc.pdf",
+    fileURI: `/ipfs/${primaryDocumentIPFSPath}`,
   };
   const ipfsHashMetaEvidenceObj = await ipfsPublish("metaEvidence.json", new TextEncoder().encode(JSON.stringify(metaevidence)));
   const metaevidenceURI = `/ipfs/${ipfsHashMetaEvidenceObj[1].hash}${ipfsHashMetaEvidenceObj[0].path}`;
@@ -45,7 +47,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId, getUnnamedA
   const ra21 = deploy(contractName, {
     from: deployer,
     gasLimit: 4000000,
-    args: [REALITIOv30[chainId], JSON.stringify(metadata), KLEROS[chainId], generateArbitratorExtraData(1, 1), metaevidenceURI],
+    args: [REALITIOv30[chainId], JSON.stringify(metadata), KLEROS[chainId], generateArbitratorExtraData(4, 5), metaevidenceURI],
   });
   console.log("Tx sent. Waiting for confirmation.");
 
